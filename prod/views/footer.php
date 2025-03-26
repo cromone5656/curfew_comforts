f4c8aabee230fc26f8cd271935607d93e24df67a
Ubuntu <ubuntu@ip-172-31-88-186.ec2.internal>
2025-03-26 01:07:07 +0000

    fixed structure

--- prod/views/footer.php
+++
@@ -1,6 +0,0 @@
-<!-- footer.php -->
-<footer>
-    <p>&copy; 2025 Curfew Comforts. All rights reserved.</p>
-</footer>
-</body>
-</html>
--- prod/views/recipe.php
+++
@@ -1,5 +0,0 @@
-<!-- views/recipe.php -->
-<h1><?php echo htmlspecialchars($recipe['name']); ?></h1>
-<p><strong>Description:</strong> <?php echo htmlspecialchars($recipe['description']); ?></p>
-<p><strong>Ingredients:</strong> <?php echo htmlspecialchars($recipe['ingredients']); ?></p>
-<p><strong>Instructions:</strong> <?php echo htmlspecialchars($recipe['instructions']); ?></p>
